4174f12372ae8b597db61e415a5d17d90f45ffab
Antonio Vera <2104277+anvera@users.noreply.github.com>
2026-07-03 18:00:32 -0700

    Basic program training on TinyStories. Slightly too high in memory.

---
+++ cs336_basics/train_bpe_tinystories.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from cs336_basics.train_bpe import train_bpe
+
+import pickle
+import os
+from datetime import datetime
+
+training_file = "./data/TinyStoriesV2-GPT4-train.txt" 
+
+vocab, merges = train_bpe(
+  training_file,
+  10000,
+  ["<|endoftest|>"]
+)
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename_vocab = os.path.join("/tmp", f"vocab_{timestamp}.pkl")
+filename_merges = os.path.join("/tmp", f"merges_{timestamp}.pkl")
+
+# Save
+with open(filename_vocab, "wb") as f:
+  pickle.dump(vocab, f)
+  print(f"Saved file {filename_vocab}")
+
+with open(filename_merges, "wb") as f:
+  pickle.dump(merges, f)
+  print(f"Saved file {filename_merges}")
+
