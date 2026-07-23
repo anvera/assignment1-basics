3a5be778e36b704c4c394b6d3b1b525c275b9c57
Antonio Vera <2104277+anvera@users.noreply.github.com>
2026-07-22 18:41:58 -0700

    Improves command line tool, starts fast, efficient version of merge.

--- cs336_basics/train_bpe_on_corpus.py
+++ cs336_basics/train_bpe_on_corpus.py
@@ -1,58 +1,92 @@
 #!/usr/bin/env python3
 
 from cs336_basics.train_bpe_multiproc import train_bpe_multiproc
-from cs336_basics.train_bpe import train_bpe
+from cs336_basics.train_bpe import train_bpe, fast_train_bpe
 
 import pickle
 import os
 from datetime import datetime
-import sys
+import argparse
+
 
 def train_bpe_on_corpus(
-  special_tokens: list[str],
-  training_file: str,
-  vocabulary_length: int,
-  multiproc: bool,
-  num_processes: int
-  ):
+    special_tokens: list[str],
+    training_file: str,
+    vocabulary_length: int,
+    multiproc: bool,
+    num_processes: int,
+    fast: bool,
+):
 
-  if multiproc:
-    vocab, merges = train_bpe_multiproc(
-      training_file,
-      vocabulary_length,
-      special_tokens,
-      num_processes
+    if multiproc:
+        vocab, merges = train_bpe_multiproc(
+            training_file, vocabulary_length, special_tokens, num_processes
+        )
+    else:
+        if fast:
+            vocab, merges = fast_train_bpe(
+                training_file, vocabulary_length, special_tokens
+            )
+        else:
+            vocab, merges = train_bpe(training_file, vocabulary_length, special_tokens)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename_vocab = os.path.join("/tmp", f"vocab_{timestamp}.pkl")
+    filename_merges = os.path.join("/tmp", f"merges_{timestamp}.pkl")
+
+    # Save
+    with open(filename_vocab, "wb") as f:
+        pickle.dump(vocab, f)
+        print(f"Saved file {filename_vocab}")
+
+    with open(filename_merges, "wb") as f:
+        pickle.dump(merges, f)
+        print(f"Saved file {filename_merges}")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Trains BPE tokenizer on a given corpus with default special tokens."
     )
-  else:
-    vocab, merges = train_bpe(
-      training_file,
-      vocabulary_length,
-      special_tokens
+    special_tokens = ["<|endoftext|>"]
+    parser.add_argument("corpus_filename")
+    parser.add_argument(
+        "-vl",
+        "--vocabulary_length",
+        type=int,
+        required=True,
+        help="The vocabulary length, including the 256 basic bytes.",
+    )
+    parser.add_argument(
+        "-f",
+        "--fast",
+        action="store_true",
+        help="Enable use of the efficient version of training.",
+    )
+    parser.add_argument(
+        "-mp",
+        "--multiproc",
+        action="store_true",
+        help="Enable parallel processing from raw corpus to pre-tokens occurrence counting.",
+    )
+    parser.add_argument(
+        "-p",
+        "--processors",
+        type=int,
+        default=4,
+        help="Number of processors used when multiproc is enabled.",
     )
 
-  timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-  filename_vocab = os.path.join("/tmp", f"vocab_{timestamp}.pkl")
-  filename_merges = os.path.join("/tmp", f"merges_{timestamp}.pkl")
+    args = parser.parse_args()
 
-  # Save
-  with open(filename_vocab, "wb") as f:
-    pickle.dump(vocab, f)
-    print(f"Saved file {filename_vocab}")
+    print(args)
 
-  with open(filename_merges, "wb") as f:
-    pickle.dump(merges, f)
-    print(f"Saved file {filename_merges}")
- 
-
-
-if __name__ == '__main__':
-
-  assert len(sys.argv) >= 4
-
-  special_tokens = ["<|endoftext|>"]
-  training_file = sys.argv[1]
-  vocabulary_length = int(sys.argv[2])
-  multiproc = True if sys.argv[3].lower() == 'true' else False
-  num_processes = int(sys.argv[4]) if multiproc else 0
-  
-  train_bpe_on_corpus(special_tokens, training_file, vocabulary_length, multiproc, num_processes) 
+    train_bpe_on_corpus(
+        special_tokens,
+        args.corpus_filename,
+        args.vocabulary_length,
+        args.multiproc,
+        args.processors,
+        args.fast,
+    )
