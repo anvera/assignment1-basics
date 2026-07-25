3097d5e2ed4e674b2c836aa83863eed651c2c943
Antonio Vera <2104277+anvera@users.noreply.github.com>
2026-07-24 17:44:47 -0700

    Updates tinystories training to allow the fast version.

--- cs336_basics/train_bpe_tinystories.py
+++ cs336_basics/train_bpe_tinystories.py
@@ -1,5 +1,9 @@
 from cs336_basics.train_bpe_on_corpus import train_bpe_on_corpus
+import argparse
 
 if __name__ == '__main__':
-
-  train_bpe_on_corpus(["<|endoftext|>"], "./data/TinyStoriesV2-GPT4-train.txt", 10000, True, 6) 
+  parser = argparse.ArgumentParser(description="BPE Training on TinyStories")
+  parser.add_argument("-f", "--fast", action="store_true", help="Use the efficient version of tokenizer training.")
+  args = parser.parse_args()
+  print(args)
+  train_bpe_on_corpus(["<|endoftext|>"], "../data/TinyStoriesV2-GPT4-train.txt", 10000, False, -1, args.fast)
